dc9ae92a9a449bc78d3c0b89db5995d835757fe0
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-28 03:55:00 +0000

    creating a new movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,17 @@
 class MoviesController < ApplicationController
+  def create_movie
+    @movie=Movie.new
+    @movie.title=params.fetch("movie_title")
+    @movie.year=params.fetch("movie_year")
+    @movie.duration=params.fetch("movie_duration")
+    @movie.description=params.fetch("movie_description")
+    @movie.image=params.fetch("movie_image")
+    @movie.director_id=params.fetch("movie_director_id")
+    
+    @movie.save
+      redirect_to("/movies", {:notice => "New Movie created successfully."})
+    end
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
@@ -14,4 +27,6 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  
 end
