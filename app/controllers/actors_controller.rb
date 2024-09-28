6dd2856f57ce879b7773eba6630c654142d82bcf
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-28 04:30:22 +0000

    created update and delete methods for actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,38 @@
 class ActorsController < ApplicationController
+
+  def delete_actor
+    @actor=Actor.where({ :id => params.fetch("path_id") }).first
+    @actor.destroy
+    redirect_to("/actors")
+  end
+
+  def update_actor
+    the_id = params.fetch("path_id")
+    @actor = Actor.where({ :id => the_id }).first
+    @actor.name = params.fetch("actor_name")
+    @actor.dob = params.fetch("actor_dob")
+    @actor.bio = params.fetch("actor_bio")
+    @actor.image = params.fetch("actor_image")
+
+    if @actor.valid?
+      @actor.save
+      redirect_to("/actors/#{@actor.id}", { :notice => "Course updated successfully."} )
+    else
+      redirect_to("/actors/#{@actor.id}", { :alert => "Course failed to update successfully." })
+    end
+
+  end
+
+  def create_actor
+    actor=Actor.new
+    actor.name=params.fetch("actor_name")
+    actor.dob=params.fetch("actor_dob")
+    actor.bio=params.fetch("actor_bio")
+    actor.image=params.fetch("actor_image")
+    actor.save
+    redirect_to("/actors")
+  end 
+
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,18 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
   post("/insert_director", { :controller => "directors", :action => "create_director" })
+  post("/delete_director/:path_id", { :controller => "directors", :action => "delete_director" })
+  post("/update_director/:path_id", { :controller => "directors", :action => "update_director" })
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
   post("/insert_movie", { :controller => "movies", :action => "create_movie" })
+  post("/delete_movie/:path_id", { :controller => "movies", :action => "delete_movie" })
+  post("/update_movie/:path_id", { :controller => "movies", :action => "update_movie" })
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
+  post("/insert_actor", { :controller => "actors", :action => "create_actor" })
+  post("/delete_actor/:path_id", { :controller => "actors", :action => "delete_actor" })
+  post("/update_actor/:path_id", { :controller => "actors", :action => "update_actor" })
 end
